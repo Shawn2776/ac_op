c6eee631c7fd2c66900f825c947070f59485d0ed
Shawn Harrington <shawn.harrington2776@gmail.com>
2025-01-19 13:51:09 -0800

    added metadata to dashboard layout

--- app/(secure)/dashboard/layout.js
+++ app/(secure)/dashboard/layout.js
@@ -2,6 +2,12 @@ import Navbar from "@/components/Navbar";
 import "../../../app/globals.css";
 import { AuthProvider } from "@/Providers";
 
+export const metadata = {
+  title: "NIC Adventure Center",
+  description:
+    "Outdoor Pursuits exercises both body and mind through wilderness-based, environmentally sound outdoor adventures. Our activities foster educational growth through self-awareness, teamwork and risk-taking. We provide a non-profit outdoor service with trips and rentals available to everyone.",
+};
+
 const Layout = ({ children }) => {
   return (
     <html lang="en">
